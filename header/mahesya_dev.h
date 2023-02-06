041cce3ef16093aa31001aeca85cd4284a6dedda
mahesyasn18 <mahesyasn18@gmail.com>
2023-02-06 09:36:25 +0700

    Menambahkan fungsi untuk menghitung akar

--- header/mahesya_dev.h
+++ header/mahesya_dev.h
@@ -1,1 +1,17 @@
-#include<iostream>
+#ifndef mahesya_dev_H
+#define mahesya_dev_H
+#include<math.h>
+
+int inputAkar(int input){
+	printf("Masukkan Angka:");
+	scanf("%d", &input);
+	return input;
+}
+
+float akar(int input){
+	return sqrt(input);
+}
+void ProsesAkar(int input, float hasil){
+	printf("hasil akar %d adalah %2f",input, hasil );
+}
+#endif
