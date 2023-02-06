98adc56dc4c8af7ae927c5ecffdc0b679b306c76
adindaraisa <adindazhr11@gmail.com>
2023-02-06 10:04:32 +0700

    nambahin header

--- header/adinda_dev.h
+++ header/adinda_dev.h
@@ -1,8 +1,19 @@
 #ifndef adinda_dev_h
 #define adinda_dev_h
 
-float perkalian(float bilangansatu, float bilangandua){
-	return bilangansatu*bilangandua;
+void inputbilangan(int *bilangansatu, int *bilangandua){
+	printf("Masukkan bilangan 1: ");
+	scanf("%d", &*bilangansatu);
+	printf("Masukkan bilangan dua: ");
+	scanf("%d", &*bilangandua);
+}
+
+int perkalian(int bilangansatu, int bilangandua){
+	return bilangansatu * bilangandua;
+}
+
+void outputbilangan(int hasil){
+	printf("Hasil: %d", hasil);
 }
 
 #endif
