6e0dd90a26d50b1e2f20e1a299481e69bbb1f3bf
Ahmad Fauzy <fauzy15546@gmail.com>
2023-02-06 10:51:45 +0700

    menambahkan file header AhmadFauzy_dev.h

---
+++ header/AhmadFauzy_dev.h
@@ -0,0 +1,19 @@
+#define AhmadFauzy_dev_h
+#ifndef AhmadFauzy_dev_h
+ 
+// function pwmbagian
+void input(int *a, int *b){
+	scanf("%d", &(*a));
+	scanf("%d", &(*b));
+}
+
+float proses(int x, int y){
+	return (x / y)
+}
+
+void output(int h, int i, float j){
+	printf(" hasil %d / %d = %1f", h,i,hasil);
+}
+
+
+#endif
