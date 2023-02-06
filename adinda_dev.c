73cf51ccac24e5800e0048c2ccb3186385c41e23
adindaraisa <adindazhr11@gmail.com>
2023-02-06 09:12:34 +0700

    menambahan header dan program adinda

---
+++ adinda_dev.c
@@ -0,0 +1,5 @@
+#include "header\adinda_dev.h"
+
+float perkalian(float bilangansatu, float bilangandua){
+	return bilangansatu*bilangandua;
+}
---
+++ header/adinda_dev.h
@@ -0,0 +1,6 @@
+#ifndef adinda_dev_h
+#define adinda_dev_h
+
+float perkalian(float bilangansatu, float bilangandua);
+
+#endif
