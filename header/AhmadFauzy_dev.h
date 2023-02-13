e607daebcd6364fe589e9c04a42ce1234f415a99
Ahmad Fauzy <fauzy15546@gmail.com>
2023-02-13 07:59:20 +0700

    haha

--- header/AhmadFauzy_dev.h
+++ header/AhmadFauzy_dev.h
@@ -1,17 +1,17 @@
-#define AhmadFauzy_dev_h
 #ifndef AhmadFauzy_dev_h
+#define AhmadFauzy_dev_h
  
 // function pwmbagian
-void input(int *a, int *b){
+void inputPembagian(int *a, int *b){
 	scanf("%d", &(*a));
 	scanf("%d", &(*b));
 }
 
-float proses(int x, int y){
-	return (x / y)
+float prosesPembagian(int x, int y){
+	return (x / y);
 }
 
-void output(int h, int i, float j){
+void outputPembagian(int h, int i, float j){
 	printf(" hasil %d / %d = %1f", h,i,hasil);
 }
 
