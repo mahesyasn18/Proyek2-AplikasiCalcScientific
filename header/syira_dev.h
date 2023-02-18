1809bc6fc27e6fac9c2cf617be29ea591aab679e
syrrkhrnsaa <syira.khoerunisa.tif22@polban.ac.id>
2023-02-18 08:59:48 +0700

    hasil revisi isi .h yang aku

--- header/syira_dev.h
+++ header/syira_dev.h
@@ -27,56 +27,67 @@ int x;
  
 int decitohexa()
 {
-	//quotient = hasil bagi
-	//remainder = sisa
-    int decimal, quotient, remainder;
+	int decimal, hasilbagi, sisa;
     int i, j = 0;
     char hexadecimal[100];
  
     printf("Enter decimal number: ");
     scanf("%d", &decimal);
-    quotient = decimal;
+    hasilbagi = decimal;
  
-    while (quotient != 0)
+    while (hasilbagi != 0)
     {
-        remainder = quotient % 16;
-        if (remainder < 10)
-            hexadecimal[j++] = 48 + remainder;
+        sisa = hasilbagi % 16;
+        if (sisa < 10)
+        {
+            hexadecimal[j++] = 48 + sisa;
+        }
         else
-            hexadecimal[j++] = 55 + remainder;
-        	quotient = quotient / 16;
+        {
+            hexadecimal[j++] = 55 + sisa;
+        }
+        	hasilbagi = hasilbagi / 16;
+        	hexadecimal[j] = hasilbagi;
     }
     // display integer into character
     	for (i = j; i >= 0; i--)
+    	{
         printf("%c",hexadecimal[i]);
+    	}
     	return 0;
 }
 
    
 int hexatodeci() {  
-    int decimalNumber=0;
-    char hexDigits[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
-      '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    char hexadecimal[30];  
-    int i, j, power=0, digit;  
-   
-    printf("Enter a Hexadecimal Number: ");  
-    scanf("%s", hexadecimal);  
-     
-    /* Converting hexadecimal number to decimal number */
-    for(i=strlen(hexadecimal)-1; i >= 0; i--) {
-        /*search currect character in hexDigits array */
-        for(j=0; j<16; j++){
-            if(hexadecimal[i] == hexDigits[j]){
-                decimalNumber += j*pow(16, power);
-            }
-        }
-        power++;
-    }  
-  
-    printf("Decimal Number : %ld", decimalNumber);  
-   
-    return 0;  
+	char hexa[20];
+	int decimal = 0, length, i, digit;
+
+   printf("enter hexadecimal number: ");
+   scanf("%s", hexa);
+
+   length = strlen(hexa);
+
+   for (i = 0; hexa[i] != '\0' ; i++)
+   {
+      length--;
+      if (hexa[i] >= '0' && hexa[i] <= '9')
+      {
+         digit = hexa[i] - '0';
+      }
+      else if (hexa[i] >= 'a' && hexa[i] <= 'f')
+      {
+         digit = hexa[i] - 'a' + 10;
+      }
+      else if (hexa[i] >= 'A' && hexa[i] <= 'F')
+      {
+         digit = hexa[i] - 'A' + 10;
+      }
+      decimal += digit * pow(16, length);
+   }
+
+   printf("%d", decimal);
+
+   return 0;
 }
 
 int main(){
