1c0080c3f47e3745c2db85da8415a55e3fd1b73b
mahesyasn18 <mahesyasn18@gmail.com>
2023-02-26 21:25:12 +0700

    fix conflict

--- header/AryaKusumah_dev.h
+++ header/AryaKusumah_dev.h
@@ -4,14 +4,9 @@
 
 
 double operasiPenjumlahan(double bilangansatu, double bilangandua){
-<<<<<<< HEAD
-	return a + b; 
-=======
 	return bilangansatu + bilangandua; 
->>>>>>> 615c0ac438d6d20c018e9d502fdb79e0ff052887
 }
 
-//operasi modulus
 double operasiModulus (double bilangansatu, double bilangandua){
     return (int)bilangansatu%(int)bilangandua;
 }
