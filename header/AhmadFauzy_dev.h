be3bfd28371257f8eabe88f4f9bc644c9918da2c
Ahmad Fauzy <fauzy15546@gmail.com>
2023-03-19 22:48:10 +0700

    push AhmadFauzy_dev.h

--- header/AhmadFauzy_dev.h
+++ header/AhmadFauzy_dev.h
@@ -9,49 +9,29 @@ double operasiPembagian(double bilangansatu, double bilangandua){
 }
 
 
-//modul turunan
 
+double operasiLogaritma(double bilangansatu, double bilangandua) {
+    double HasilPembagian = bilangandua;
+    double sisa;
+    double Hasil = 0;
 
-
-double operasiLogaritma(double bilangansatu, double bilangandua){
-	if (bilangandua <= 0 || bilangansatu <= 0) {
+    if (bilangandua <= 0 || bilangansatu <= 0) {
         printf("Invalid input\n");
+        return 0.0/0.0;
+    } else if (bilangansatu == bilangandua) {
         return 1;
+    } else {
+        while (HasilPembagian >= bilangansatu) {
+                HasilPembagian /= bilangansatu;
+                Hasil += 1;
+        }
+        if(HasilPembagian != 1){
+        	Hasil+=HasilPembagian/bilangansatu;
+		}
     }
 
-	 return log(bilangandua) / log(bilangansatu);
+    return Hasil;
 }
-
-
-
-//konversi biner
-int decimalToBinary(int *decimal)
-{
-    int binary;
-    int i = 0;
-    int j;
-  
-    binary = *decimal % 2;
-    *decimal = *decimal / 2;
-    return binary;
-}
-
-//int main()
-//{
-//    int decimal,bin[32],i,j;
-//    i=0;
-//    j=0;
-//    
-//    printf("masukan angka: ");
-//    scanf("%d", &decimal);
-//    while(decimal>0){
-//    bin[i] = decimalToBinary(&decimal);
-//    i++;
-//	}
-//	printf("Binar dari angka tersebut adalah: ");
-//    for(j = i - 1; j >= 0; j--) {
-//	printf("%d",bin[j]);
-//	}
 
 
 #endif
